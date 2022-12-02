9882d815857b48ab69ef6c1f116bdf7aa753e287
Fernando Xavier <sfexavier@gmail.com>
2022-12-02 17:00:50 -0300

    copiando enunciados

--- projetoTargetSistemas/Program.cs
+++ projetoTargetSistemas/Program.cs
@@ -4,8 +4,21 @@ using System.Globalization;
 ////===============================================
 /*
 1) Resposta: 91;
+Observe o trecho de código abaixo:
 
+int INDICE = 13, SOMA = 0, K = 0;
+
+enquanto K < INDICE faça
+{
+K = K + 1;
+SOMA = SOMA + K;
+}
+
+imprimir(SOMA);
+
+Ao final do processamento, qual será o valor da variável SOMA?
 */
+
 // int INDICE = 13, SOMA = 0, K = 0;
 // while (K < INDICE)
 // {
@@ -15,7 +28,10 @@ using System.Globalization;
 // Console.WriteLine(SOMA);
 ////===============================================
 /*
-2) 
+2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
+
+IMPORTANTE:
+Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código;
 
 */
 
@@ -46,7 +62,13 @@ using System.Globalization;
 // Console.WriteLine(pertence ? "O número PERTENCE á sequência" : "O número NÃO PERTENCE á sequência");
 // ===============================================
 /*
-3) 
+3) Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa, na linguagem que desejar, que calcule e retorne:
+• O menor valor de faturamento ocorrido em um dia do mês;
+• O maior valor de faturamento ocorrido em um dia do mês;
+• Número de dias no mês em que o valor de faturamento diário foi superior à média mensal.
+IMPORTANTE:
+a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
+b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;
 
 */
 // String conteudoArquivo = File.ReadAllText("Arquivos/dados.json");
@@ -94,7 +116,15 @@ using System.Globalization;
 // Console.WriteLine($"No mês, tiveram {quantidadeDiasMaioresFaturamento} dias, que o faturamento foi maior que a média mensal - Média mensal:{mediaMensal.ToString("C", CultureInfo.CurrentCulture)}");
 // ===============================================
 /*
-4) 
+4) Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
+
+SP – R$67.836,43
+RJ – R$36.678,66
+MG – R$29.229,88
+ES – R$27.165,48
+Outros – R$19.849,53
+
+Escreva um programa na linguagem que desejar onde calcule o percentual de representação que cada estado teve dentro do valor total mensal da distribuidora.
 
 */
 // double sp = 67836.43;
@@ -120,7 +150,11 @@ using System.Globalization;
 // Console.WriteLine($"Porcentagem de faturamento mensal de Outros foi de: {porcentagemOTS.ToString("0")}% - Valor {outros.ToString("C", CultureInfo.CurrentCulture)}");
 // ===============================================
 /*
-5) 
+5) Escreva um programa que inverta os caracteres de um string.
+
+IMPORTANTE:
+a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+b) Evite usar funções prontas, como, por exemplo, reverse;
 
 */
 // Console.Write("Digite uma palavra: ");
